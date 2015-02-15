dd4a4f04cdfc7a3e5ba01b88c6142fe51c601b5d
Yeshi.jp <yoshikema@gmail.com>
2015-02-15 15:26:49 +0900

    動いたら音楽が自動的に鳴り始めるように

--- app.js
+++ app.js
@@ -1,39 +1,33 @@
+var mp3file
 window.addEventListener("load", function() {
+
+    //audio要素に曲名をセット
+    mp3file = $("input[name='songSelect']:checked").val();
+    if (!mp3file){ mp3file = "ageage_01.mp3" };
+    $("#mainAudio").attr("src","audio/"+mp3file);
+
     $("#statusTxt").text("接続中...");
     loadApi();
 
-    //loaded event
+    var sid = setInterval( loadApi,1000 );
+
+    /*
     $("#bigButton").on("touchend",function(){
-        //console.log($("input[name='songSelect']:checked").val());
-        
-        //つながってない時
-        if(! $("#mainAudio").hasClass("player")){
-
-            //つなげに行く
-            loadApi();
-
-        //準備完了の時
-        }else if($("#mainAudio").hasClass("stop")){
-
-            //audio要素に曲名をセット
-            $("#mainAudio").attr("src","audio/"+$("input[name='songSelect']:checked").val());
-            mainAudio.play();
-
-        }else if($("#mainAudio").hasClass("stop")){
-            mainAudio.pause();
-        }
+        stopSound();
     });
+    */
+
 });
 
+/*
 for (var i in songs){
     var o = songs[i];
     var text = o.text;
     $("#main").append("<label><input type='radio' name='songSelect' value=\"" + o.filename + "\">" + text + "</label>");
 }
+*/
 
 function loadApi(){
-
-  $("#bigButton").addClass("connecting");
 
   var dataUrl = "http://192.168.1.20:8080/api/getCSC";
 
@@ -42,17 +36,48 @@ function loadApi(){
   req.onreadystatechange = function(aEvt){
       if(req.readyState == 4){
           if(req.status == 200){
-              console.log(req.responseText);
+              //console.log(req.responseText);
+              dataSuccess(JSON.parse(req.responseText));
           }else{
-              console.log("NG",req.status);
+              //console.log("NG",req.status);
+              dataError(req.status);
           }
-      }else{
-          console.log("error");
       }
   };
   req.send(null);
 
 }
  
+function dataSuccess(data){
+    $("#statusTxt").text("接続済み");
+    $("#statusMark").addClass("active");
+    $("#bigButton").removeClass("connecting");
+    $("#bigButton").addClass("player");
+    $("#resData").text(data.speed);
+    if(data.speed > 2 ){
+    //if(data.speed >= 0 ){
+        playSound();
 
+    }else{
+        stopSound();
+
+    }
+}
+function dataError(errstat){
+    $("#statusTxt").text("接続エラー");
+    $("#statusMark").removeClass("active");
+    $("#bigButton").removeClass("connecting player stop");
+    $("#resData").text("status: "+ errstat);
+}
+
+function playSound(){
+    if(! $("#bigButton").hasClass("stop")){
+        mainAudio.play();
+        $("#bigButton").addClass("stop");
+    }
+}
+function stopSound(){
+    mainAudio.pause();
+    $("#bigButton").removeClass("stop");
+}
 
--- songlist.js
+++ songlist.js
@@ -1,5 +1,5 @@
 var songs = new Array(
-{id:01,filename:"ageage_01.mp3",text:"ハイテンポな曲"},
+{id:01,filename:"ageage_01.mp3",text:"ノリノリな曲"},
 {id:02,filename:"nonbiri_01.mp3",text:"のんびりな曲"}/*,
 {id:03,filename:""},
 {id:04,filename:""},*/
